4279d02859e4e27dd8d5d31e096f1404ff517997
Valiantsin Lutchanka <lutchenkovalentin@gmail.com>
2022-09-18 00:52:39 +0200

    rework github actions

--- wdio-github.conf.js
+++ wdio-github.conf.js
@@ -2,14 +2,14 @@ const basicConfig = require('./wdio.conf')
 exports.config = {
   ...basicConfig.config,
   capabilities: [
-    // {
-    //   maxInstances: 3,
-    //   browserName: 'chrome',
-    //   acceptInsecureCerts: true,
-    //   'goog:chromeOptions': {
-    //     args: ['--headless', '--disable-gpu', '--disable-dev-shm-usage', '--window-size=1920,1080'],
-    //   },
-    // },
+    {
+      maxInstances: 3,
+      browserName: 'chrome',
+      acceptInsecureCerts: true,
+      'goog:chromeOptions': {
+        args: ['--headless', '--disable-gpu', '--disable-dev-shm-usage', '--window-size=1920,1080'],
+      },
+    },
     {
       maxInstances: 3,
       browserName: 'MicrosoftEdge',
